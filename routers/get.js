17eb5d7bf3aa570cf667d4a852cf3e0ba90681f7
tothmTheCoder23 <tothm.113sz@acsjszki.hu>
2022-11-20 11:12:52 +0100

    Handle all 404 error

--- routers/get.js
+++ routers/get.js
@@ -4,13 +4,30 @@ const router = express.Router();
 const data = require("../public/data");
 const dataPerformer = require("../my_modules/performerIndex");
 
+/**
+ * main page
+ */
 router.get("/", (req, res) => {
   res.render("pages/index", { title: "Songgenerator - Main", performers: data });
 });
 
+/**
+ * If no results, redirect to main page
+ */
 router.get("/choosed", (req, res) => {
   let performerQuery = req.query["performer"];
-  res.render("pages/choosed", { title: "Songgenerator - Choosed performer", printPerformer: dataPerformer(performerQuery)[0], displayAvatas: dataPerformer(performerQuery)[1], song: dataPerformer(performerQuery)[2], url: dataPerformer(performerQuery)[3] });
+  try {
+    res.render("pages/choosed", { title: "Songgenerator - Choosed performer", printPerformer: dataPerformer(performerQuery)[0], displayAvatas: dataPerformer(performerQuery)[1], song: dataPerformer(performerQuery)[2], url: dataPerformer(performerQuery)[3] });
+  } catch (error) {
+    res.redirect("/");
+  }
+});
+
+/**
+ * If refer to wrong page, redirect to main page
+ */
+router.get("/*", (req, res) => {
+  res.redirect("/");
 });
 
 module.exports = router;
