fdb616db23147e24ebdaf10505873cda38a4b13b
tothmTheCoder23 <tothm.113sz@acsjszki.hu>
2022-11-25 15:29:33 +0100

    Edit the search in the choosed site

--- public/src/choosed.js
+++ public/src/choosed.js
@@ -1,1 +1,79 @@
-document.getElementById("myfooter").classList.add("fixed-bottom");
+/**
+ * performs the search
+ */
+function search() {
+  // the searched value
+  const searchedSong = document.getElementById("search-bar").value.toLowerCase();
+
+  // the searched songs
+  let searchedSongs = [];
+
+  // the searched urls
+  let searchedUrls = [];
+
+  // if there is match, runs up to fullNamesIndex
+  for (let j = 0; j < data[performerIndex()].song.length; j++) {
+    if (data[performerIndex()].song[j].toLowerCase().startsWith(searchedSong)) {
+      searchedSongs.push(data[performerIndex()].song[j]);
+      searchedUrls.push(data[performerIndex()].url[j]);
+    }
+  }
+
+  displayCharacters(searchedSongs, searchedUrls);
+}
+
+let data = [];
+
+// Fetch API  https://www.w3schools.com/jsref/api_fetch.asp
+async function loadData() {
+  const res = await fetch("../data.json");
+  data = await res.json();
+  displayCharacters(data[performerIndex()].song, data[performerIndex()].url);
+  document.getElementById("myfooter").classList.add("fixed-bottom");
+}
+
+/**
+ * display the songs and urls in a list
+ * @param {*} songs
+ * @param {*} urls
+ */
+function displayCharacters(songs, urls) {
+  let txt = "";
+
+  for (let i = 0; i < songs.length; i++) {
+    txt +=
+      `
+      <li class="list-group-item songs pb-3">
+      <a href="` +
+      urls[i] +
+      `" target="_blank">` +
+      songs[i] +
+      `</a>
+    </li>
+    </div>
+    `;
+  }
+  document.getElementById("song").innerHTML = txt;
+}
+
+loadData();
+
+/**
+ * The searched performers index
+ * @returns
+ */
+function performerIndex() {
+  // the searched performer
+  const performer = document.getElementById("choosedPerformer").innerHTML;
+
+  // collect all names to an array
+  const fullNames = [];
+  for (let i = 0; i < data.length; i++) {
+    fullNames.push(data[i].full_name);
+  }
+
+  // get the index of actual performer
+  const fullNamesIndex = fullNames.indexOf(performer);
+
+  return fullNamesIndex;
+}
