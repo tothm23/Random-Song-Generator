088848ec7d304b39d254b5281d698e309876b0ca
tothmTheCoder23 <tothm.113sz@acsjszki.hu>
2022-11-26 13:09:22 +0100

    Edit the links
    The test was success!

--- public/src/index.js
+++ public/src/index.js
@@ -12,8 +12,6 @@ function getRandomNumber(max) {
  */
 function getRandomPerformer() {
   const randomPerformer = data[getRandomNumber(data.length)].short_name;
-  //window.location.href = `https://randomsonggenerator.herokuapp.com/choosed?performer=${randomPerformer}`;
-
   /*
   if (window.location.href.endsWith("#")) {
     window.location.href = window.location.href.slice(0, window.location.href.length - 1);
@@ -73,11 +71,7 @@ function displayCharacters(characters) {
   const mycard = document.getElementsByClassName("mycard");
   for (let i = 0; i < mycard.length; i++) {
     mycard[i].addEventListener("click", () => {
-      //window.location.href = `https://randomsonggenerator.herokuapp.com/choosed?performer=${characters[i].short_name}`;
-
       window.location.href = `${window.location.href}choosed?performer=${characters[i].short_name}`;
-
-      //window.location.href = `http://127.0.0.1:8080/choosed?performer=${characters[i].short_name}`;
     });
   }
 }
