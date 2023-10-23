68ce7b189c1e8203024568a92a20b261cae78967
tothm23 <tothm.113sz@acsjszki.hu>
2023-10-23 18:33:02 +0200

    Add HttpClientModule to imports array

--- song-generator/src/app/app.module.ts
+++ song-generator/src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
