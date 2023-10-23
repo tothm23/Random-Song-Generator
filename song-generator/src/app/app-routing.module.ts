ec748531411ec0658e6ccde2f535c139a461cb81
tothm23 <tothm.113sz@acsjszki.hu>
2023-10-23 19:01:11 +0200

    Add wildcard path

--- song-generator/src/app/app-routing.module.ts
+++ song-generator/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './login/login.component';
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: 'login' },
 ];
 
 @NgModule({
