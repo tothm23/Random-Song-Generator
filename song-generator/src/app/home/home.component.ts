49074bee851e263ec6b0fa6a8dc64626c25f5e3b
tothm23 <tothmilan224@gmail.com>
2023-10-26 12:48:20 +0200

    Add greeting to home page

--- song-generator/src/app/home/home.component.ts
+++ song-generator/src/app/home/home.component.ts
@@ -5,4 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  getGreeting(): string {
+    const date = new Date();
+    let hour = date.getHours();
+
+    if (hour >= 0 && hour < 5) {
+      return 'Good night';
+    } else if (hour < 12) {
+      return 'Good morning';
+    } else if (hour < 18) {
+      return 'Good day';
+    } else if (hour < 22) {
+      return 'Good evening';
+    } else {
+      return 'Good night';
+    }
+  }
+}
