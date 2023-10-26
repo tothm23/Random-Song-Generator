0ac42354457ecfa7bcee16c8fd6df34123d08258
tothm23 <tothmilan224@gmail.com>
2023-10-26 13:02:20 +0200

    Implement subscribePlaylistsItemsSync method

--- song-generator/src/app/home/home.component.ts
+++ song-generator/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ErrorService } from '../services/error.service';
+import { RequestService } from '../services/request.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  seed_artists!: string;
+  seed_tracks!: string;
+
   isFetchingRecommendedArtists!: boolean;
 
+  constructor(
+    private requestService: RequestService,
+    private errorService: ErrorService
+  ) {}
+
   async ngOnInit() {
+    this.seed_artists = '';
+    this.seed_tracks = '';
+
     this.isFetchingRecommendedArtists = false;
+
+    if (navigator.onLine) {
+      await this.subscribePlaylistsItemsSync();
+    } else {
+      this.errorService.setError('No internet connection.');
+    }
   }
 
   getGreeting(): string {
@@ -27,5 +46,29 @@ export class HomeComponent implements OnInit {
     } else {
       return 'Good night';
     }
+  }
+
+  async subscribePlaylistsItemsSync() {
+    const artistsLimited = [];
+    const tracks = [];
+
+    try {
+      const data = await this.requestService
+        .getPlaylistsItems(0, 5)
+        .toPromise();
+
+      for (let i = 0; i < data.items.length; i++) {
+        tracks.push(data.items[i].track.id);
+        for (let j = 0; j < data.items[i].track.artists.length; j++) {
+          artistsLimited.push(data.items[i].track.artists[j].id);
+        }
+      }
+    } catch (error: any) {
+      this.errorService.handleError(error);
+    }
+
+    // Separating with a comma and deleting the last comma
+    this.seed_artists = artistsLimited.join(',');
+    this.seed_tracks = tracks.join(',');
   }
 }
