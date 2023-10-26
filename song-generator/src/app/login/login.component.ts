da77b7b1d98b70ff111134ea5b4244f317b09c82
tothm23 <tothmilan224@gmail.com>
2023-10-26 11:53:12 +0200

    Create login button

--- song-generator/src/app/login/login.component.ts
+++ song-generator/src/app/login/login.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { ErrorService } from '../services/error.service';
+import { SpotifyService } from '../services/spotify.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  constructor(
+    private spotifyService: SpotifyService,
+    private errorService: ErrorService
+  ) {}
+
+  onLogin() {
+    if (navigator.onLine) {
+      this.spotifyService.setSpoitfyAuthUrl();
+      this.spotifyService.redirectToSpoitfyAuth();
+    } else {
+      this.errorService.setError('No internet connection.');
+    }
+  }
+}
