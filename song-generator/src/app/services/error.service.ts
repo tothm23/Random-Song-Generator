a6c2b831dc11fd1c40ca18c1f25c19e1c9c2af30
tothm23 <tothmilan224@gmail.com>
2023-10-26 11:50:59 +0200

    Implement handleError method

--- song-generator/src/app/services/error.service.ts
+++ song-generator/src/app/services/error.service.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -12,5 +13,21 @@ export class ErrorService {
 
   setError(error: string) {
     this.error = error;
+  }
+
+  handleError(error: HttpErrorResponse | any) {
+    if (error.status === 401) {
+      this.setError('Bad or expired token.');
+    } else if (error.status === 403) {
+      this.setError('Bad OAuth request.');
+    } else if (error.status === 429) {
+      this.setError('The app has exceeded its rate limits.');
+    } else if (error.status === 504) {
+      this.setError('Gateway Timeout.');
+    } else if (error.status === 0) {
+      this.setError('No internet connection.');
+    } else {
+      this.setError(error);
+    }
   }
 }
