f4122f608bbca401f54fae0ddb40731e4c051a79
tothm23 <tothmilan224@gmail.com>
2023-10-27 13:32:18 +0200

    Create necessary variables

--- song-generator/src/app/playlist/playlist.component.ts
+++ song-generator/src/app/playlist/playlist.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Song } from '../models/song';
 
 @Component({
   selector: 'app-playlist',
   templateUrl: './playlist.component.html',
   styleUrls: ['./playlist.component.css'],
 })
-export class PlaylistComponent {}
+export class PlaylistComponent {
+  searchedSong!: string;
+  isPlaying!: boolean;
+  isSelected!: boolean;
+  isFetching!: boolean;
+
+  songs!: Song[];
+
+  selectedItem!: any;
+  currentTime!: Subject<number>;
+  playlistItemsOffset!: number;
+
+  @ViewChild('player', { static: true }) player!: ElementRef;
+}
