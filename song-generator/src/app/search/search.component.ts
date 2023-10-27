76de71a6ea1f7690ba629f968d0f351065fb0ae2
tothm23 <tothmilan224@gmail.com>
2023-10-27 13:00:43 +0200

    Create search bar

--- song-generator/src/app/search/search.component.ts
+++ song-generator/src/app/search/search.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { RequestService } from '../services/request.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css'],
 })
-export class SearchComponent {}
+export class SearchComponent {
+  query!: string;
+
+  constructor(private requestService: RequestService) {}
+
+  searchArtists() {
+    this.requestService
+      .getAllArtists(this.query, 5)
+      .subscribe((data: any) => {});
+  }
+}
