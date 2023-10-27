a0907b8e060fd4657b1ff40fc61e6db49a049612
tothm23 <tothmilan224@gmail.com>
2023-10-27 13:03:44 +0200

    Add result box

--- song-generator/src/app/search/search.component.ts
+++ song-generator/src/app/search/search.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RequestService } from '../services/request.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ErrorService } from '../services/error.service';
 
 @Component({
   selector: 'app-search',
@@ -8,12 +10,19 @@ import { RequestService } from '../services/request.service';
 })
 export class SearchComponent {
   query!: string;
+  data: any;
 
-  constructor(private requestService: RequestService) {}
+  constructor(
+    private requestService: RequestService,
+    private errorService: ErrorService
+  ) {}
 
   searchArtists() {
-    this.requestService
-      .getAllArtists(this.query, 5)
-      .subscribe((data: any) => {});
+    this.requestService.getAllArtists(this.query, 5).subscribe((data: any) => {
+      this.data = data;
+    }),
+      (error: HttpErrorResponse) => {
+        this.errorService.handleError(error);
+      };
   }
 }
