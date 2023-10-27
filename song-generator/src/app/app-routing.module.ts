e932112a1afd12308d5ae2cccba2f01c7be7d498
tothm23 <tothmilan224@gmail.com>
2023-10-27 12:45:36 +0200

    Add search path

--- song-generator/src/app/app-routing.module.ts
+++ song-generator/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'search', component: SearchComponent },
   { path: '**', redirectTo: 'login' },
 ];
 
