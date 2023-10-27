d2a2ceab32a40bba3810474ae1b919a84a63507d
tothm23 <tothmilan224@gmail.com>
2023-10-27 13:30:32 +0200

    Add playlist path

--- song-generator/src/app/app-routing.module.ts
+++ song-generator/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
+import { PlaylistComponent } from './playlist/playlist.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'search', component: SearchComponent },
+  { path: 'playlist', component: PlaylistComponent },
   { path: '**', redirectTo: 'login' },
 ];
 
