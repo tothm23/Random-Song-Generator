772fb20f62ad248f089624bd46c0605e3f4e9a14
tothm23 <tothmilan224@gmail.com>
2023-10-29 09:54:21 +0100

    Create loading spinner

--- song-generator/src/app/artists/artists.component.ts
+++ song-generator/src/app/artists/artists.component.ts
@@ -7,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArtistsComponent implements OnInit {
   searchedItem!: string;
+  isFetchingArtists!: boolean;
 
   ngOnInit() {
     this.searchedItem = '';
+    this.isFetchingArtists = false;
   }
 }
